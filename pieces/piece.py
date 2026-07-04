54786759a6ed3e4978bf2583c1d4c0745592ee6d
vessbon <115010176+vessbon@users.noreply.github.com>
2026-07-04 20:38:45 +0200

    fix(pieces): update type hint for color attribute

--- pieces/piece.py
+++ pieces/piece.py
@@ -6,14 +6,14 @@ from chess_types import Color, Coordinate
 
 class Piece(ABC):
     def __init__(self, color: Color) -> None:
-        self.color = color
+        self.color: Color = color
 
     @abstractmethod
     def symbol(self) -> str:
         pass
 
     @abstractmethod
-    def moves(self, position: Coordinate, board) -> List[Coordinate]:
+    def moves(self, position: Coordinate) -> List[Coordinate]:
         pass
 
     def __str__(self) -> str:
