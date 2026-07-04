8ddf540db2874d398ae42165e4ae5fba7ba73ba0
vessbon <115010176+vessbon@users.noreply.github.com>
2026-07-04 21:11:28 +0200

    refactor(main): cleanup main

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
 from board import Board
-from pieces import Pawn
 
 
 def main():
     board = Board()
 
     board.setup()
-
-    board.set(3, 2, Pawn(color="white"))
-    print(board.select(1, 3))
-    print(board.select(4, 2))
 
     print(f"\n{board}\n")
 
