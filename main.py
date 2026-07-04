17d6056afa3d48ba3b296b0aa4e7f5ca18722e91
vessbon <115010176+vessbon@users.noreply.github.com>
2026-07-04 20:26:20 +0200

    fix(main): correct import statement and improve main function structure

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
-from board.board import Board
+from board import Board
+from pieces import Pawn
 
 
 def main():
-    Board()
+    board = Board()
+
+    board.setup()
+
+    board.set(3, 2, Pawn(color="white"))
+    print(board.select(1, 3))
+    print(board.select(4, 2))
+
+    print(f"\n{board}\n")
 
 
 if __name__ == "__main__":
