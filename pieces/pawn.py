cab0a3b47962e8d57e8988f587f9f606466c8a41
vessbon <115010176+vessbon@users.noreply.github.com>
2026-07-04 20:27:25 +0200

    feat(pieces): implement moves method for Pawn and update Piece class signature

--- pieces/pawn.py
+++ pieces/pawn.py
@@ -5,5 +5,8 @@ class Pawn(Piece):
     def __init__(self, color) -> None:
         super().__init__(color)
 
-    def symbol(self) -> str:
+    def symbol(self):
         return "P" if self.color == "white" else "p"
+
+    def moves(self, position, board):
+        return [(1, 2)]
--- pieces/piece.py
+++ pieces/piece.py
@@ -1,17 +1,20 @@
 from abc import ABC, abstractmethod
+from typing import List
+
+from chess_types import Color, Coordinate
 
 
 class Piece(ABC):
-    def __init__(self, color) -> None:
+    def __init__(self, color: Color) -> None:
         self.color = color
 
     @abstractmethod
     def symbol(self) -> str:
         pass
 
-    """ @abstractmethod
-    def moves(self, position, board):
-        pass """
+    @abstractmethod
+    def moves(self, position: Coordinate, board) -> List[Coordinate]:
+        pass
 
     def __str__(self) -> str:
         return self.symbol()
