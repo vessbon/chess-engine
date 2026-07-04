b1fca45e00e380c1525ad869f9205b8ed639ae0f
vessbon <115010176+vessbon@users.noreply.github.com>
2026-07-05 01:15:40 +0200

    feat(bishop): add Bishop piece implementation with movement logic

--- pieces/__init__.py
+++ pieces/__init__.py
@@ -1,7 +1,8 @@
+from .bishop import Bishop
 from .king import King
 from .pawn import Pawn
 from .piece import Piece
 from .queen import Queen
 from .rook import Rook
 
-__all__ = ["Piece", "Pawn", "King", "Queen", "Rook"]
+__all__ = ["Piece", "Pawn", "King", "Queen", "Rook", "Bishop"]
