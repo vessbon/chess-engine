bdb86a3087bc744b570ca88e27ee170c0c26ecd9
vessbon <115010176+vessbon@users.noreply.github.com>
2026-07-05 01:04:16 +0200

    feat(piece): implement sliding move logic for pieces and refactor Queen moves method

--- pieces/queen.py
+++ pieces/queen.py
@@ -7,27 +7,4 @@ class Queen(Piece):
         return "Q" if self.color == "white" else "q"
 
     def moves(self, position, board):
-        row, col = position
-
-        moves = []
-
-        for dr, dc in ALL_DIRECTIONS:
-            next_row = row + dr
-            next_col = col + dc
-
-            while board.in_bounds(next_row, next_col):
-                move = (next_row, next_col)
-                occupant = board.get(*move)
-
-                if occupant and occupant.color == self.color:
-                    break
-
-                moves.append(move)
-
-                if occupant:
-                    break
-
-                next_row += dr
-                next_col += dc
-
-        return moves
+        return self._sliding_moves(position, board, ALL_DIRECTIONS)
