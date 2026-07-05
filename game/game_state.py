e700c33b77799ff8a38bb7a6d364a471f3d4aafb
vessbon <115010176+vessbon@users.noreply.github.com>
2026-07-05 20:47:51 +0200

    fix(game): update string representation of current color in GameState

--- game/game_state.py
+++ game/game_state.py
@@ -75,7 +75,7 @@ class GameState:
         )
 
         return (
-            f"{self.current_color} to move\n"
+            f"{self.current_color.value} to move\n"
             f"{self.time_left / 1000} seconds left\n"
             f"white can{'' if white_castling_rights else 'not'} castle\n"
             f"black can{'' if black_castling_rights else 'not'} castle\n"
