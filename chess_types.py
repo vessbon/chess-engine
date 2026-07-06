7d4005969c29dc42b65827966c5208c916d3a845
vessbon <115010176+vessbon@users.noreply.github.com>
2026-07-06 23:10:08 +0200

    feature(color): add opposite property to Color enum for color inversion

--- chess_types.py
+++ chess_types.py
@@ -5,5 +5,9 @@ class Color(Enum):
     WHITE = "white"
     BLACK = "black"
 
+    @property
+    def opposite(self):
+        return Color.BLACK if self is Color.WHITE else Color.WHITE
+
 
 type Coordinate = tuple[int, int]
